5149fe68eb1e460e91af41005691691c0ecb69f6
fares-admin <fares.sys.vn@gmail.com>
2023-07-13 07:56:42 +0700

    modify review entity

--- review-service/src/repository/review-repository/review-entity.ts
+++ review-service/src/repository/review-repository/review-entity.ts
@@ -8,9 +8,9 @@ export enum ReviewStatus {
 export class Review {
   _id: mongoose.Types.ObjectId = new mongoose.Types.ObjectId()
 
-  productId: mongoose.Types.ObjectId[] = []
+  productId: mongoose.Types.ObjectId = new mongoose.Types.ObjectId()
 
-  variantId: mongoose.Types.ObjectId = new mongoose.Types.ObjectId()
+  variantId: mongoose.Types.ObjectId[] = []
 
   email: string = ''
 
@@ -20,15 +20,19 @@ export class Review {
 
   reviewer: mongoose.Types.ObjectId = new mongoose.Types.ObjectId()
 
+  created: Date = new Date()
+
   status: ReviewStatus = ReviewStatus.OFFLINE
 }
 
 export const ReviewSchema = new mongoose.Schema({
   _id: mongoose.Types.ObjectId,
-  targetId: Array<mongoose.Types.ObjectId>,
+  productId: mongoose.Types.ObjectId,
+  variantId: Array<mongoose.Types.ObjectId>,
   email: String,
   name: String,
   score: Number,
   reviewer: mongoose.Types.ObjectId,
+  created: Date,
   status: String,
 })
