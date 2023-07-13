f7f2e0a7c5fc589b13f2c505555b319f41749553
fares-admin <fares.sys.vn@gmail.com>
2023-07-13 18:52:16 +0700

    add validation room

---
+++ review-service/src/service/review-service/room-entity.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+export class Room {
+  _id: mongoose.Types.ObjectId = new mongoose.Types.ObjectId()
+
+  productId: mongoose.Types.ObjectId = new mongoose.Types.ObjectId()
+
+  title: string = ''
+
+  size: number = 0
+
+  maxPersons: number = 0
+
+  price: number = 0
+
+  salePrices: number = 0
+
+  image: string = ''
+
+  active: boolean = true
+}
--- room-service/src/service/room-service/room-service-function/add-room-function.ts
+++ room-service/src/service/room-service/room-service-function/add-room-function.ts
@@ -1,8 +1,11 @@
 import { Room } from '@/src/repository/room-repository/room-entity'
 import { RoomRepository } from '@/src/repository/room-repository/room-repository'
+import axios from 'axios'
 import { CommonResponse } from 'common-abstract-fares-system'
+import { generateServiceToken } from 'common-lib-fares-system'
 import { convertValue } from 'object-mapper-fares-system'
 import { validate } from 'validation-tool-fares-system'
+import { ProductEntity } from '../product-entity'
 import { RoomReq, RoomReqError, RoomValidatorSchema } from '../room-req'
 
 export const addNewRoomFunction = async (
@@ -18,29 +21,46 @@ export const addNewRoomFunction = async (
       status: 400,
     }
   }
-  // const res = {
-  //   success: false,
-  //   message: '',
-  //   result: {
-  //     productId: '',
-  //     title: '',
-  //     size: '',
-  //     maxPersons: '',
-  //     price: '',
-  //     salePrices: '',
-  //     image: '',
-  //   },
-  //   status: 400,
-  // }
-
-  // to do
-  // const findProduct = await repository.findOne('productId', req.productId)
-  // if (!findProduct.result) {
-  //   return {
-  //     ...res,
-  //     result: { ...res.result, productId: 'Invalid product' },
-  //   }
-  // }
+  const internalToken = generateServiceToken({ serviceName: process.env.SERVICE_NAME || '' })
+  const callInternalProduct = await axios.get(
+    `${process.env.PRODUCT_SERVICE_URL}/api/service/findProduct?id=${req.productId}&ServiceToken=${internalToken}`
+  )
+  const res = {
+    success: false,
+    message: '',
+    result: {
+      productId: '',
+      title: '',
+      size: '',
+      maxPersons: '',
+      price: '',
+      salePrices: '',
+      image: '',
+    },
+    status: 400,
+  }
+  if (callInternalProduct.status !== 200)
+    return {
+      status: 500,
+      message: 'server error',
+      result: '',
+      success: false,
+    }
+  const result = callInternalProduct.data as CommonResponse<ProductEntity | string>
+  if (!result.success) {
+    return {
+      ...res,
+      message: result.message,
+      result: { ...res.result, productId: 'invalid productId' },
+    }
+  }
+  const findTitle = await repository.findOne('title', req.title)
+  if (findTitle.result) {
+    return {
+      ...res,
+      result: { ...res.result, title: 'title existed' },
+    }
+  }
   const entity = convertValue<Room>(req, new Room())
   const { error } = await repository.insert([{ ...entity }])
   if (error) {
